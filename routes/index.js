42419c242c3a00565ded7f8b4c313bda290a65fd
satanishota <88267507+satanishota@users.noreply.github.com>
2022-11-11 23:37:13 +0900

    13th

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ランニングコース' });
-  res.render('index', { sample1: public/sample1.png })
+  res.render('index', { sample1: '/public/sample1.png' })
 });
 
 module.exports = router;
